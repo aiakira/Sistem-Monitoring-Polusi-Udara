bf3d0bdf8074f10b6d947ae456713e2b9ac1943d
aiakira <164518885+aiakira@users.noreply.github.com>
2025-02-10 17:05:56 +0800

    Add files via upload

---
+++ sql/monitoring_polusi.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 10, 2025 at 09:00 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `monitoring_polusi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `polutan_data`
+--
+
+CREATE TABLE `polutan_data` (
+  `id` int NOT NULL,
+  `waktu` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `pm25` decimal(5,2) DEFAULT NULL,
+  `pm10` decimal(5,2) DEFAULT NULL,
+  `kelembaban` decimal(5,2) DEFAULT NULL,
+  `suhu` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sensor_data`
+--
+
+CREATE TABLE `sensor_data` (
+  `id` int NOT NULL,
+  `mq2` decimal(10,2) NOT NULL,
+  `mq7` decimal(10,2) NOT NULL,
+  `waktu` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `polutan_data`
+--
+ALTER TABLE `polutan_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sensor_data`
+--
+ALTER TABLE `sensor_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `polutan_data`
+--
+ALTER TABLE `polutan_data`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+
+--
+-- AUTO_INCREMENT for table `sensor_data`
+--
+ALTER TABLE `sensor_data`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
